e9f441665d4e872a9275d91fba7c620716c1baa5
aveetron <aveechakra@gmail.com>
2020-04-19 01:04:40 +0600

    doner submit , datatable list show done

--- bank/settings.py
+++ bank/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'donor',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'bank.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATIC_URL = '/assets/'
+
+STATICFILES_DIRS = [
+                        os.path.join(BASE_DIR, 'assets')
+]
--- donor/views.py
+++ donor/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponseRedirect
+from .models import BloodDonor
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib import messages
+from datetime import datetime
 # Create your views here.
+
+
+def home(request):
+    allDonor = BloodDonor.objects.filter(status = True)
+    context = {
+        'allDonor': allDonor
+    }
+    return render(request, 'index.html', context)
+
+
+@csrf_exempt
+def addDonor(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        blood = request.POST.get('blood')
+        phone = request.POST.get('phone')
+        batch = request.POST.get('batch')
+        address = request.POST.get('address')
+
+        bloodDonor = BloodDonor(
+            name = name,
+            blood_group = blood,
+            phone = phone,
+            batch = batch,
+            address = address,
+            status = True,
+            created_date = datetime.now()
+        )
+        bloodDonor.save()
+        message = 'Donor uploaded, need admin approval'
+        messages.success(request, message)
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
